60c5b97172201939c2d0c33a823682e60094fe51
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-06-03 01:15:42 -0400

    working: colsing slider front page

--- components/BodySlider.tsx
+++ components/BodySlider.tsx
@@ -26,7 +26,7 @@ const BodySlider = () => {
         transition={{
           duration: 0.4,
           ease: "easeInOut",
-          delay: 0.3,
+          delay: 0.4,
         }}
         exit={{ opacity: 0, transition: { duration: 0.2 } }}
       >
