eb3340199700b02751e6dd28761481b6e8a4fb00
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-06-07 15:02:07 -0400

    styled: padding layout general

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -66,7 +66,7 @@ const Layout: NextPage<Iprops> = ({ children }) => {
         </motion.div>
 
         <div
-          className={`fixed top-0 right-0  h-full px-[50px] ${
+          className={`fixed top-0 right-0 h-full pr-4 xl:pr-12 ${
             router.asPath === "/" ? "flex" : "hidden"
           }`}
         >
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -75,7 +75,7 @@ const Navigation: NextPage<IProps> = ({
 
   return (
     <div className="fixed top-0 left-0 flex h-full  items-center justify-start  py-48 sm:pr-[0vw] lg:pr-[40vw] xl:pr-[0vw]">
-      <div className="flex h-full items-center px-[50px]">
+      <div className="flex h-full items-center pl-12">
         <motion.ul
           variants={stagger}
           initial="hidden"
