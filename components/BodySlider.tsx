a6364ab41709a411820d976ead1bf67eb53ceeca
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-06-09 23:08:21 -0400

    added: drag gesture

--- components/BodySlider.tsx
+++ components/BodySlider.tsx
@@ -4,6 +4,7 @@ import { use100vh } from "react-div-100vh";
 import { Context } from "../components/Context";
 import { Geo } from "../utils/geoMetrical";
 import useRamdomColors from "../utils/useRandomColors";
+import { useWindowSize } from "../utils/hooks";
 
 const BodySlider = () => {
   const appContext = useContext(Context);
@@ -12,6 +13,8 @@ const BodySlider = () => {
 
   const rectangle = Geo().rectangle;
   const polygon = Geo().polygon;
+
+  const width = useWindowSize().width;
 
   const actionSlider = () => {
     switch (true) {
@@ -26,6 +29,10 @@ const BodySlider = () => {
 
   return (
     <motion.div
+      drag={width! < 680 ? "x" : false}
+      dragConstraints={{ left: 0, right: 100 }}
+      dragElastic={0.1}
+      dragSnapToOrigin
       layout="position"
       layoutId="sliderWrapper"
       initial={{
