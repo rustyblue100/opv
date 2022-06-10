e85709ce135d53a73d97561b3ec747faab38673b
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-06-10 12:43:11 -0400

    styled: spotligths ipad 2

--- components/SpotLights.tsx
+++ components/SpotLights.tsx
@@ -38,7 +38,7 @@ const Spotlights: NextPage<IProps> = ({ menuHover }) => {
           delay: 1.8,
         }}
         exit="exit"
-        className="col-span-1 -ml-8 hidden max-w-[55px] will-change-auto sm:block sm:max-w-[75px] md:max-w-[75px] 2md:max-w-[100px] lg:-ml-24 lg:max-w-[155px] xl:-ml-36 xl:max-w-[200px] 3xl:max-w-full iphone_landscape:max-w-[50px]"
+        className="col-span-1 -ml-8 hidden max-w-[55px] will-change-auto sm:block sm:max-w-[75px] md:max-w-[75px] 2md:max-w-[100px] lg:-ml-24 lg:max-w-[155px] xl:-ml-36 xl:max-w-[200px] 2xl:max-w-[260px] 3xl:max-w-full iphone_landscape:max-w-[50px]"
       >
         <Image
           src="https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvNDM3LWZlbGl4LTE1NS5qcGc.jpg"
@@ -64,7 +64,7 @@ const Spotlights: NextPage<IProps> = ({ menuHover }) => {
           delay: 0.4,
         }}
         exit="exit"
-        className="col-span-3 max-w-[80px] justify-self-end will-change-auto xxs:max-w-[130px] xs:max-w-[200px] sm:col-span-2 sm:max-w-[150px] md:max-w-[160px] 2md:max-w-[180px] lg:max-w-[240px] xl:max-w-[320px] 3xl:max-w-full iphone_landscape:max-w-[110px]  ipadPro:max-w-[320px]"
+        className="col-span-3 max-w-[80px] justify-self-end will-change-auto xxs:max-w-[130px] xs:max-w-[200px] sm:col-span-2 sm:max-w-[150px] md:max-w-[160px] 2md:max-w-[180px] lg:max-w-[240px] xl:max-w-[320px] 2xl:max-w-[460px] 3xl:max-w-full iphone_landscape:max-w-[110px]  ipadPro:max-w-[320px]"
       >
         <Image
           src="https://fastly.4sqi.net/img/general/width960/54255444_x7ZweBJGKxWeqQXQcxE4KRkRWWL2l-PhKbhyQcDnwBY.jpg"
@@ -90,7 +90,7 @@ const Spotlights: NextPage<IProps> = ({ menuHover }) => {
           delay: 1.1,
         }}
         exit="exit"
-        className="col-span-2 hidden max-w-[80px] will-change-auto sm:block sm:max-w-[100px] md:max-w-[110px] 2md:max-w-[120px] lg:ml-4 lg:max-w-[190px] xl:max-w-[220px] 3xl:max-w-full  iphone_landscape:max-w-[100px]"
+        className="col-span-2 hidden max-w-[80px] will-change-auto sm:block sm:max-w-[100px] md:max-w-[110px] 2md:max-w-[120px] lg:ml-4 lg:max-w-[190px] xl:max-w-[220px] 2xl:max-w-[320px] 3xl:max-w-full  iphone_landscape:max-w-[100px]"
       >
         <Image
           src="https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdXB3azYyMTk1ODI5LXdpa2ltZWRpYS1pbWFnZS1rb3dzNmcxZi5qcGc.jpg"
