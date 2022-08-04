b905aa5e83f42d10350f0f827e481594c1708ac6
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-08-04 14:22:05 -0400

    removed title from _app

--- __mocks__/sanityClient.js
+++ __mocks__/sanityClient.js
@@ -1,146 +1,61 @@
 export const elementProps = [
-  {
-    _id: "225eb49d-312e-4126-8c98-de095616f234",
-    artiste: null,
-    complet: true,
-    date: "2022-07-28T16:00:00.000Z",
-    description: {
-      _type: "localeBlock",
-      en: [
-        {
-          _key: "92729a73972a",
-          _type: "block",
-          children: [
-            {
-              _key: "7d986d459eb60",
-              _type: "span",
-              marks: [],
-              text: "\n",
-            },
-            {
-              _key: "7d986d459eb61",
-              _type: "span",
-              marks: ["strong", "b0ca7b466399"],
-              text: "LES MERCREDIS FUNK AVEC #LECYPHER",
-            },
-            {
-              _key: "7d986d459eb62",
-              _type: "span",
-              marks: [],
-              text: "\n",
-            },
-          ],
-          markDefs: [
-            {
-              _key: "b0ca7b466399",
-              _type: "link",
-              href: "http://www.urbsci.com/lecypher",
-            },
-          ],
-          style: "normal",
-        },
-        {
-          _key: "3eec6dbe067d",
-          _type: "block",
-          children: [
-            {
-              _key: "5c58f41eb1ce0",
-              _type: "span",
-              marks: [],
-              text: "Every URBAN SCIENCE #LECYPHER show is totally unique, pushing the boundaries of the future of hip-hop. This live band creates uninterrupted sets on the fly, all while keeping the dancefloor moving and the imagination racing. With every performance, instrumental and vocal virtuosity of the highest degree are on display, from blazing guitar solos to epic freestyle raps.",
-            },
-          ],
-          markDefs: [],
-          style: "normal",
-        },
-        {
-          _key: "8c35c33086f4",
-          _type: "block",
-          children: [
-            {
-              _key: "63d9ff62a16a0",
-              _type: "span",
-              marks: [],
-              text: "https://youtu.be/GFb_oo3OSmk",
-            },
-          ],
-          markDefs: [],
-          style: "normal",
-        },
-      ],
-      fr: [
-        {
-          _key: "b0789ee0cbf5",
-          _type: "block",
-          children: [
-            {
-              _key: "b35b47ec2b070",
-              _type: "span",
-              marks: [],
-              text: "Chaque spectacle du Urban Science #LeCypher est totalement unique, repoussant les limites du futur du hip-hop. Ce live band crée des prestations dynamiques, dramatiques et sans arrêt dans le moment, tout en gardant la piste de danse en action et inspirant l’imagination. Avec chaque prestation, une virtuosité instrumentale et vocale du plus haut degré est exposée, des solos de guitare flamboyants aux raps freestyle épiques.",
-            },
-          ],
-          markDefs: [],
-          style: "normal",
-        },
-      ],
-    },
-    mainImage: {
-      _type: "image",
-      asset: {
-        _ref: "image-bdeaf1d5905d04810dc4eced7cb9f0149b3f3da2-885x999-jpg",
-        _type: "reference",
-      },
-    },
-    prix: 12,
-    recurrents: null,
-    slug: "2022-07-28-les-mercredis-funk-avec-lecypher",
-    title: {
-      _type: "localeString",
-      fr: "LES MERCREDIS FUNK AVEC #LECYPHER",
-    },
-  },
-  {
-    _id: "0edda81b-7e2f-4c72-aacd-8031c3637e18",
-    artiste: null,
-    complet: null,
-    date: "2022-07-31T19:15:00.000Z",
-    description: null,
-    mainImage: null,
-    prix: 20,
-    recurrents: {
-      artiste: [
-        {
-          _createdAt: "2022-05-12T20:09:11Z",
-          _id: "cdd15562-4664-436f-9063-41bb9077ae0f",
-          _rev: "dbTx7Zg3RHkVbMMwyVPZyH",
-          _type: "artiste",
-          _updatedAt: "2022-05-12T20:09:11Z",
-          description: "Contrebasse",
-          nom: "Rémi-Jean Leblanc",
-        },
-        {
-          _createdAt: "2022-05-12T20:08:55Z",
-          _id: "0a2da459-7110-4d78-bd0a-ee15d84178c3",
-          _rev: "dbTx7Zg3RHkVbMMwyWymrS",
-          _type: "artiste",
-          _updatedAt: "2022-05-13T16:08:18Z",
-          instrument: "Piano",
-          nom: "Jonathan Cayer",
-        },
-      ],
+  [
+    {
+      _id: "225eb49d-312e-4126-8c98-de095616f234",
+      artiste: null,
+      complet: true,
+      date: "2022-08-16T00:45:00.000Z",
       description: {
         _type: "localeBlock",
         en: [
           {
-            _key: "66f5df9fe01a",
+            _key: "92729a73972a",
             _type: "block",
             children: [
               {
-                _key: "486100d2ec0d0",
+                _key: "7d986d459eb61",
+                _type: "span",
+                marks: ["strong"],
+                text: "LES MERCREDIS FUNK AVEC #LECYPHER",
+              },
+              {
+                _key: "7d986d459eb62",
                 _type: "span",
                 marks: [],
-                text: "EN ------Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies feugiat volutpat. Vestibulum lacinia iaculis commodo. Ut auctor tellus sodales lacus iaculis semper. In faucibus enim erat, sit amet convallis ligula varius ut. Aliquam vel leo at nisi elementum pretium ac non metus. Nulla ex mauris, placerat mattis magna sed, tempor maximus erat. Nulla fermentum velit eu diam posuere lobortis.",
+                text: "\n",
+              },
+            ],
+            markDefs: [
+              {
+                _key: "86f381065b3e",
+                _type: "link",
+              },
+            ],
+            style: "normal",
+          },
+          {
+            _key: "3eec6dbe067d",
+            _type: "block",
+            children: [
+              {
+                _key: "5c58f41eb1ce0",
+                _type: "span",
+                marks: [],
+                text: "Every URBAN SCIENCE #LECYPHER show is totally unique, pushing the boundaries of the future of hip-hop. This live band creates uninterrupted sets on the fly, all while keeping the dancefloor moving and the imagination racing. With every performance, instrumental and vocal virtuosity of the highest degree are on display, from blazing guitar solos to epic freestyle raps.",
+              },
+            ],
+            markDefs: [],
+            style: "normal",
+          },
+          {
+            _key: "8c35c33086f4",
+            _type: "block",
+            children: [
+              {
+                _key: "63d9ff62a16a0",
+                _type: "span",
+                marks: [],
+                text: "https://youtu.be/GFb_oo3OSmk",
               },
             ],
             markDefs: [],
@@ -149,14 +64,42 @@ export const elementProps = [
         ],
         fr: [
           {
-            _key: "d9561b71a2e2",
+            _key: "b0789ee0cbf5",
             _type: "block",
             children: [
               {
-                _key: "a6327a3cfc240",
+                _key: "b35b47ec2b070",
                 _type: "span",
                 marks: [],
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies feugiat volutpat. Vestibulum lacinia iaculis commodo. Ut auctor tellus sodales lacus iaculis semper. In faucibus enim erat, sit amet convallis ligula varius ut. Aliquam vel leo at nisi elementum pretium ac non metus. Nulla ex mauris, placerat mattis magna sed, tempor maximus erat. Nulla fermentum velit eu diam posuere lobortis.",
+                text: "Chaque spectacle du Urban Science #LeCypher est totalement unique, repoussant les limites du futur du hip-hop. Ce live band crée des prestations dynamiques, dramatiques et sans arrêt dans le moment, tout en gardant la piste de danse en action et inspirant l’imagination. Avec chaque prestation, une virtuosité instrumentale et vocale du plus haut degré est exposée, des solos de guitare flamboyants aux raps freestyle épiques.",
+              },
+            ],
+            markDefs: [],
+            style: "normal",
+          },
+          {
+            _key: "ac157bb40b49",
+            _type: "block",
+            children: [
+              {
+                _key: "307667f4cae2",
+                _type: "span",
+                marks: [],
+                text: "",
+              },
+            ],
+            markDefs: [],
+            style: "normal",
+          },
+          {
+            _key: "4f741b03c71d",
+            _type: "block",
+            children: [
+              {
+                _key: "a9ea7a22e64c",
+                _type: "span",
+                marks: [],
+                text: "",
               },
             ],
             markDefs: [],
@@ -164,89 +107,108 @@ export const elementProps = [
           },
         ],
       },
+      facebook: "https://www.facebook.com/mbamtl",
       mainImage: {
         _type: "image",
         asset: {
-          _ref: "image-95969865daa358b7b73c4eb54243367b7d405d0b-1500x2000-jpg",
+          _ref: "image-bdeaf1d5905d04810dc4eced7cb9f0149b3f3da2-885x999-jpg",
           _type: "reference",
         },
       },
-      slug: null,
+      prix: 12,
+      recurrents: null,
+      site: "https://www.mbam.qc.ca/fr/",
+      slug: "2022-08-15-les-mercredis-funk-avec-lecypher",
       title: {
         _type: "localeString",
-        fr: "Jam session avec le big Band orchestra",
+        fr: "Les Mercredis Funk Avec #lecypher",
       },
+      video: "https://www.youtube.com/watch?v=-IrrS036mcM",
     },
-    slug: "2022-07-31-jam-session-avec-le-big-band-orchestra",
-    title: null,
-  },
-  {
-    _id: "e1a1e3a2-f3e7-433c-843d-7a5f96a7657e",
-    artiste: null,
-    complet: true,
-    date: "2022-08-13T04:00:00.000Z",
-    description: null,
-    mainImage: null,
-    prix: 25,
-    recurrents: {
-      artiste: [
-        {
-          _createdAt: "2022-05-12T20:09:28Z",
-          _id: "5837c445-13f2-43e3-bca7-7f4f02525932",
-          _rev: "mS4B3BB5OZSuYJ2u038mVm",
-          _type: "artiste",
-          _updatedAt: "2022-05-13T16:23:38Z",
-          instrument: "Batterie",
-          mainImage: {
-            _type: "image",
-            asset: {
-              _ref: "image-0b3cac7ee26e817dfff6d6b264a7cb06d419260c-491x463-jpg",
-              _type: "reference",
-            },
-          },
-          nom: "Al Bourgeois",
-          photo: {
-            _type: "image",
-            asset: {
-              _ref: "image-d1121e1d220e323b16cb78d30d41fbcabc65eed2-2316x3088-jpg",
-              _type: "reference",
-            },
-          },
-        },
-        {
-          _createdAt: "2022-05-12T20:08:35Z",
-          _id: "8f557ebf-38a3-48c4-b032-146c5192c251",
-          _rev: "dbTx7Zg3RHkVbMMwyWzYBn",
-          _type: "artiste",
-          _updatedAt: "2022-05-13T16:23:45Z",
-          instrument: "Trompette & Compos",
-          nom: "Jacques Kuba Séguin",
-          photo: {
-            _type: "image",
-            asset: {
-              _ref: "image-0b3cac7ee26e817dfff6d6b264a7cb06d419260c-491x463-jpg",
-              _type: "reference",
-            },
-          },
-        },
-      ],
+    {
+      _id: "0edda81b-7e2f-4c72-aacd-8031c3637e18",
+      artiste: null,
+      complet: null,
+      date: "2022-08-19T02:15:00.000Z",
       description: null,
-      mainImage: {
-        _type: "image",
-        asset: {
-          _ref: "image-5436f7839d21dec827f242f02caebfb914897a8b-462x307-jpg",
-          _type: "reference",
+      facebook: null,
+      mainImage: null,
+      prix: 20,
+      recurrents: {
+        artiste: [
+          {
+            _createdAt: "2022-05-12T20:09:11Z",
+            _id: "cdd15562-4664-436f-9063-41bb9077ae0f",
+            _rev: "dbTx7Zg3RHkVbMMwyVPZyH",
+            _type: "artiste",
+            _updatedAt: "2022-05-12T20:09:11Z",
+            description: "Contrebasse",
+            nom: "Rémi-Jean Leblanc",
+          },
+          {
+            _createdAt: "2022-05-12T20:08:55Z",
+            _id: "0a2da459-7110-4d78-bd0a-ee15d84178c3",
+            _rev: "dbTx7Zg3RHkVbMMwyWymrS",
+            _type: "artiste",
+            _updatedAt: "2022-05-13T16:08:18Z",
+            instrument: "Piano",
+            nom: "Jonathan Cayer",
+          },
+        ],
+        description: {
+          _type: "localeBlock",
+          en: [
+            {
+              _key: "66f5df9fe01a",
+              _type: "block",
+              children: [
+                {
+                  _key: "486100d2ec0d0",
+                  _type: "span",
+                  marks: [],
+                  text: "EN ------Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies feugiat volutpat. Vestibulum lacinia iaculis commodo. Ut auctor tellus sodales lacus iaculis semper. In faucibus enim erat, sit amet convallis ligula varius ut. Aliquam vel leo at nisi elementum pretium ac non metus. Nulla ex mauris, placerat mattis magna sed, tempor maximus erat. Nulla fermentum velit eu diam posuere lobortis.",
+                },
+              ],
+              markDefs: [],
+              style: "normal",
+            },
+          ],
+          fr: [
+            {
+              _key: "d9561b71a2e2",
+              _type: "block",
+              children: [
+                {
+                  _key: "a6327a3cfc240",
+                  _type: "span",
+                  marks: [],
+                  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies feugiat volutpat. Vestibulum lacinia iaculis commodo. Ut auctor tellus sodales lacus iaculis semper. In faucibus enim erat, sit amet convallis ligula varius ut. Aliquam vel leo at nisi elementum pretium ac non metus. Nulla ex mauris, placerat mattis magna sed, tempor maximus erat. Nulla fermentum velit eu diam posuere lobortis.",
+                },
+              ],
+              markDefs: [],
+              style: "normal",
+            },
+          ],
+        },
+        mainImage: {
+          _type: "image",
+          asset: {
+            _ref: "image-95969865daa358b7b73c4eb54243367b7d405d0b-1500x2000-jpg",
+            _type: "reference",
+          },
+        },
+        slug: null,
+        title: {
+          _type: "localeString",
+          fr: "Jam session avec le big Band orchestra",
         },
       },
-      slug: "test-recurrent",
-      title: {
-        _type: "localeString",
-        fr: "Test récurrent",
-      },
+      site: null,
+      slug: "2022-08-18-jam-session-avec-le-big-band-orchestra",
+      title: null,
+      video: null,
     },
-    slug: "2022-08-13-test-recurrent",
-    title: null,
-  },
+  ],
 ];
 
 export const elementPropsPhotos = [
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import Layout from "../components/Layout/GeneralLayout";
 import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import { useEffect, useRef } from "react";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   const usePreviousRoute = () => {
@@ -20,22 +19,16 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   };
 
   const meta = { title: "Ø Tmp late", description: "Ø Template!" };
+
   return (
-    <>
-      <Head>
-        <title>{meta.title}</title>
-        <meta name="author" content="John Doe" />
-      </Head>{" "}
-      <Layout previousRoute={usePreviousRoute()} meta={meta}>
-        <AnimatePresence
-          initial={router.asPath === "/" ? true : true}
-          exitBeforeEnter
-          /*    onExitComplete={() => window.scrollTo(0, 0)} */
-        >
-          <Component {...pageProps} key={router.asPath} />
-        </AnimatePresence>
-      </Layout>
-    </>
+    <Layout previousRoute={usePreviousRoute()} meta={meta}>
+      <AnimatePresence
+        initial={router.asPath === "/" ? true : true}
+        exitBeforeEnter
+      >
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
+    </Layout>
   );
 }
 
